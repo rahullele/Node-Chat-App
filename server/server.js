a4fabdf48caa1555c4cbc6a4e12887265438f46b
rahullele <rlele@usc.edu>
2018-07-11 16:44:32 -0700

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const path=require('path');
 const express=require('express');
-
+const http=require('http');
+const socketIO=require('socket.io');
 
 //console.log(__dirname+'../public'); //This goes inside the server folder, then comes out and then goes into public folder
 const publicPath=path.join(__dirname,'../public'); //This goes directly into the public folder
 const port=process.env.PORT || 3000;
 var app=express();
+
+//Behind the scenes, express uses a built-in module called 'http'.
+//We configure express to work with http, so that we can use socket.io
+
+var server=http.createServer(app);
+var io=socketIO(server); //In io, we get the web sockets server, we can use it for emitting or listening to events.
 
 //To serve static files such as images, CSS files, and JavaScript files,
 //use the express.static built-in middleware function in Express.
@@ -15,6 +22,15 @@ var app=express();
 // The root argument specifies the root directory from which to serve static assets.
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+console.log('New user connected');
+
+socket.on('disconnect',()=>{
+console.log('User is Disconnected');
+});
+
+}); //lets you register an event listener.
+
+server.listen(port,()=>{
   console.log(`Server is up on port ${port}`);
 });
