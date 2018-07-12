e59e4268fdedb1de17c122a4efb31b7b8fde2b3e
rahullele <rlele@usc.edu>
2018-07-11 18:01:26 -0700

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+var socket = io(); //Here the client is requesting the server to open a web socket and keep that connection open.
+      //The above loaded library 'socket.io.js' is a web socket library for the client side.
+socket.on('connect',function() {
+console.log('Connected to Server'); //Arrow functions don't work on certain browsers like
+                                    //some versions of safari and firefox
+                                    //so we use regular functions on client side
+                                    //but we can use arrow functions on server side
+socket.emit('createMsg',{
+
+to:'jen',
+text:'Hey. This is Andrew.'
+});
+});
+
+socket.on('disconnect',function() {
+console.log('Disconnected from server');
+});
+
+socket.on('newMsg',function(msg) {
+console.log('New Message', msg);
+});
--- server/server.js
+++ server/server.js
@@ -25,6 +25,17 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 console.log('New user connected');
 
+socket.emit('newMsg',{
+  from:'mike',
+  text:'Hey. What is going on?',
+  createAt:123
+});
+
+socket.on('createMsg',(newMsg)=>{  //This name of event 'createMsg' should be exactly same as the one in the index.js.
+//The event names should be exactly same in client and server
+console.log('createMessage',newMsg);
+});
+
 socket.on('disconnect',()=>{
 console.log('User is Disconnected');
 });
