0c665768b244dfcb253ed4533477b90be25672ef
rahullele <rlele@usc.edu>
2018-07-12 23:26:12 -0700

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -13,24 +13,53 @@ console.log('Disconnected from server');
 });
 
 socket.on('newMessage',function(message) {
-var formattedTime=moment(message.createdAt).format('h:mm a');
-console.log('New Message', message);
+  var formattedTime=moment(message.createdAt).format('h:mm a');
+  var template=jQuery('#message-template').html(); //returns the html ie paragraph tag under id:message-template
+  var html=Mustache.render(template,{
+    text:message.text,
+    from:message.from,
+    createdAt:formattedTime,
+    timeStamp:moment(message.createdAt).fromNow()
+  });
 
-var li=jQuery('<li></li>');
-li.text(`${message.from} ${formattedTime}: ${message.text}`);
-jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+// var formattedTime=moment(message.createdAt).format('h:mm a');
+// console.log('New Message', message);
+//
+// var li=jQuery('<li></li>');
+// li.text(`${message.from} ${formattedTime}: ${message.text}`);
+// jQuery('#messages').append(li);
 });
 
+function updateTimeStamps(){
+  let loop = document.getElementById("messages").getElementsByClassName("message").length;
+  for(let i=0; i<loop; i++){
+    let timeStamp = document.getElementById("messages").getElementsByClassName("message")[i].getAttribute("data-timestamp");
+    document.getElementById("messages").getElementsByClassName("message")[i].getElementsByClassName("timestamps")[0].textContent = moment(timeStamp).fromNow();
+  }
+  console.log('TimeStamps updated');
+};
+
+setInterval(updateTimeStamps, 60000);
+
 socket.on('newLocationMessage',function(message){
-var formattedTime1=moment(message.createdAt).format('h:mm a');
+var formattedTime=moment(message.createdAt).format('h:mm a');
 
-  var li=jQuery('<li></li>');
-  var a=jQuery('<a target="_blank">My Current Location</a>');
-
-  li.text(`${message.from} ${formattedTime1}: `);
-  a.attr('href',message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+var template=jQuery('#location-message-template').html(); //returns the html ie paragraph tag under id:message-template
+var html=Mustache.render(template,{
+  url:message.url,
+  from:message.from,
+  createdAt:formattedTime,
+  timeStamp:moment(message.createdAt).fromNow()
+  // var li=jQuery('<li></li>');
+  // var a=jQuery('<a target="_blank">My Current Location</a>');
+  //
+  // li.text(`${message.from} ${formattedTime1}: `);
+  // a.attr('href',message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
+});
+jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit',function(e){
